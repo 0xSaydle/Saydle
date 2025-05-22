6624a8c9abbc596fb7d4baf01b46e554e48737a2
Wisdom Adele <adelewisdom@gmail.com>
2025-05-22 22:03:55 +0100

    moved supabase keys to server

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: import("next").NextConfig = {
   /* config options here */
   env: {
     SUPABASE_URL: process.env.SUPABASE_URL as string,
-    SUPABASE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY as string,
   },
 
