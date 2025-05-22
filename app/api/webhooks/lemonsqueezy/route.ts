675b9a80c4f5d3ec7a2a7f769be67a07b6591ec5
Wisdom Adele <adelewisdom@gmail.com>
2025-05-22 13:23:49 +0100

    fixed some linter errors affecting build

--- app/api/webhooks/lemonsqueezy/route.ts
+++ app/api/webhooks/lemonsqueezy/route.ts
@@ -105,7 +105,7 @@ export async function POST(request: NextRequest) {
           subscription_status: status,
         }).eq("id", userId)
         .select();
-
+      console.log(data);
       if (error) {
         console.error("Error updating user:", error);
       }
