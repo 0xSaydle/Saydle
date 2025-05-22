d6c3d46b71983c670cf57def3f0df8ed7b123625
Wisdom Adele <adelewisdom@gmail.com>
2025-05-22 21:59:17 +0100

    moved supabase keys to server

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig: import('next').NextConfig =
-{
-    /* config options here */
-
+const nextConfig: import("next").NextConfig = {
+  /* config options here */
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL as string,
+    SUPABASE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY as string,
+  },
 
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
-  };
+};
 
 export default nextConfig;
